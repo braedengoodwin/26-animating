59bda83c7b8ecc29093886858c264ce8cdd452ed
braedengoodwin <braedenagoodwin@hotmail.com>
2024-07-15 18:53:34 -0500

    finished animation section

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,24 +1,46 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import cityImg from '../assets/city.jpg';
-import heroImg from '../assets/hero.png';
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import cityImg from "../assets/city.jpg";
+import heroImg from "../assets/hero.png";
 
 export default function WelcomePage() {
+  const { scrollY } = useScroll();
+
+  // first array is number of pixels scrolled, second array is the value for things opacity, y post, x position, etc
+  const yCity = useTransform(scrollY, [0, 200], [0, -100]);
+  const opacityCity = useTransform(
+    scrollY,
+    [0, 300, 400, 800],
+    [1, 0.5, 0.5, 0]
+  );
+  const yHero = useTransform(scrollY, [0, 200, 400, 600], [0, -150, -300, -600]);
+  const opacityHero = useTransform(scrollY, [0, 300, 500], [1, 1, 0]);
+  const yText = useTransform(scrollY, [0, 200, 300, 500], [0, 50, 50, 300]);
+  const scaleText = useTransform(scrollY, [0, 500], [1, 1.5]);
+
   return (
     <>
       <header id="welcome-header">
-        <div id="welcome-header-content">
+        <motion.div style={{ scale: scaleText, y: yText }} id="welcome-header-content">
           <h1>Ready for a challenge?</h1>
           <Link id="cta-link" to="/challenges">
             Get Started
           </Link>
-        </div>
-        <img
+        </motion.div>
+        <motion.img
+          style={{ opacity: opacityCity, y: yCity }}
           src={cityImg}
           alt="A city skyline touched by sunlight"
           id="city-image"
         />
-        <img src={heroImg} alt="A superhero wearing a cape" id="hero-image" />
+        <motion.img
+          style={{ opacity: opacityHero, y: yHero }}
+          src={heroImg}
+          alt="A superhero wearing a cape"
+          id="hero-image"
+        />
       </header>
       <main id="welcome-content">
         <section>
